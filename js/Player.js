74eeb6e199a17ff9e2d7e62b3d85a50c8653b274
skulath-1 <103314965+skulath-1@users.noreply.github.com>
2022-07-17 11:20:57 +0100

    Add files via upload

---
+++ js/Player.js
@@ -0,0 +1,24 @@
+class Player {
+  constructor() {
+    this.name = null;
+    this.index = null;
+    this.positionX = 0;
+    this.positionY = 0;
+  }
+
+  getCount(){
+    var playerCountRef = database.ref("playerCount")
+    playerCountRef.on("value", function(data){
+    playerCount = data.val()
+    })
+  }
+  
+  updateCount(count){
+    database.ref("/").update({
+      playerCount: count 
+    }
+    )
+  }
+
+}
+
